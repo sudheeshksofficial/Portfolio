b44b53c6db766edd0cb9543399d10188b74bfbbb
sudheeshksofficial <sudheeshksofficial@gmail.com>
2022-12-04 10:36:44 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//  =========== typing animation ==========
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Software Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
